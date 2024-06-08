ee9effc6560d8a75990d6d4ce85b744cb31548a6
Kick Bataowski <saicharan14996@gmail.com>
2024-06-08 20:23:41 +0530

    link broken issue fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-           SIGNUP NOW
+            to="/docs/introduction">
+          View More
           </Link>
         </div>
       </div>
